69dcf62b221d6a60f3f9e27408f79f3094dbee33
sasiwimol <istudents@iClassrooms-iMac-18.local>
2015-01-16 16:09:46 +0700

    Project1

---
+++ Project1.playground/section-1.swift
@@ -0,0 +1,24 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+let tutorialTeam = 56
+let editorialTeam = 23
+var totalTeam = tutorialTeam + editorialTeam
+totalTeam += 1
+let priceIntInfererd = -19
+let priceIntExplicit:Int = -19
+
+let priceInferres = -19.99
+let priceExplicit:Double = -19.99
+
+let priceFloatInferred = -19.99
+let priceFloatExplicit:Float = -19.99
+
+let onSaleInferred = true
+let onSaleExplicit:Bool = false
+
+let nameInferred = "Whoopie Cushion"
+let nameExplicit:String = "Whoopie Cushion"
+
+
