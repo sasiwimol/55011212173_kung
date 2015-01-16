c721d1ff1953f715b7099b44278ecd738acb4817
sasiwimol <istudents@iClassrooms-iMac-18.local>
2015-01-16 16:42:36 +0700

    arrayCalulator

---
+++ TestFor.playground/section-1.swift
@@ -0,0 +1,14 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+var array1 = ["abc","def","cdf"]
+for (var i = 0; i < array1.count; i++){
+    println(array1[i])
+}
+
+var array2 = ["a","b","c"]
+var str = ""
+for str in array2 {
+    println(str)
+}
