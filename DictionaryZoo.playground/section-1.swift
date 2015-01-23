819d5e66f616893cb23ac2a5d53a3ea9063d011c
sasiwimol <istudents@iClassrooms-iMac-21.local>
2015-01-23 15:29:05 +0700

    ArrayZoo

---
+++ DictionaryZoo.playground/section-1.swift
@@ -0,0 +1,25 @@
+// Playground - noun: a place where people can play
+
+import UIKit
+
+class Zoo {
+    let animal: [String] = ["Tiger","Monkey","Panda"]
+    let food: [String] = ["meat","banana","Bamboo"]
+    let animal1: String
+    
+    init(animal:String){
+        self.animal1 = animal
+    }
+    func Zoofunny() -> String{
+        var anser:String = ""
+        for(var i=0; i < animal.count ; i++){
+            if(animal[i] == animal1){
+                anser = food[i];
+            }
+        }
+        return anser
+    }
+}
+
+let Zoo1 = Zoo(animal: "Panda")
+Zoo1.Zoofunny()
