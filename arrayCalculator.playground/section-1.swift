ef754b5e0bc65994d7b20317b750c1a317959ac5
sasiwimol <istudents@iClassrooms-iMac-21.local>
2015-01-23 16:21:26 +0700

    ArrayCal

--- arrayCalculator.playground/section-1.swift
+++ arrayCalculator.playground/section-1.swift
@@ -2,36 +2,4 @@
 
 import UIKit
 
-class TipCalculator{
-    
-    let total: Double
-    let taxPct: Double
-    let subtotal: Double
-    
-    init(total:Double,taxPct:Double){
-        self.total=total
-        self.taxPct=taxPct
-        subtotal = total / (taxPct + 1)
-    }
-    
-    func calcTipWithTipPct(tipPct:Double) -> Double{
-        return subtotal * tipPct
-        
-    }
-
-    func returnPossibleTip() -> [Int: Double] {
-        let possibleTipsInferred = [0.15, 0.18, 0.20]
-        let possibleTipsExplicit:[Double] = [0.15, 0.18, 0.20]
-        var numberOfItem = possibleTipsInferred.count
-        
-        var retval = [Int: Double]()
-        for possibleTip in possibleTipsInferred{
-            let intPct = Int(possibleTip*100)
-            retval[intPct] = calcTipWithTipPct(possibleTip)
-        }
-        return retval
-    }
-
-}
-let tipCalc = TipCalculator(total: 33.25, taxPct: 0.06)
-tipCalc.returnPossibleTip()
+var str = "Hello, playground"
